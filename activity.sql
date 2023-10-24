bab29dcdc890ab690a3f4f98f7c59d9bf2149781
MAnikaev <anikaev.maks@gmail.com>
2023-10-24 11:00:57 +0300

    Добавлена кр (1 лвл)

--- activity.sql
+++ activity.sql
@@ -1,1 +1,26 @@
----
+with recursive activity_hierarchy as (
+    select
+        a.id,
+        a.parent_id,
+        a.activitytype_id,
+        a.activity_id,
+        at.name as activity_name,
+        cast(at.name as varchar) as "path"
+    from activitytype as at, activity as a
+    where a.parent_id is null 
+    	and at.id = a.activitytype_id
+
+    union
+
+    select
+        a.id,
+        a.parent_id,
+        a.activitytype_id,
+        a.activity_id,
+        at."name" as activity_name,
+        concat(h.path, ' -> ', at."name") as "path"
+    from activity as a
+		join activitytype as at on at.id = a.activitytype_id
+		join activity_hierarchy as h on a.parent_id = h.id
+)
+select * from activity_hierarchy order by (id);
--- data.sql
+++ data.sql
@@ -1,17 +1,54 @@
-    insert into activitytype(
-    	  activitytypeid
-        ,name
-        ,sysname
-    )
-    select d.*
-    from (
-    	values(1,'Программа','Program')
-        ,(2,'Подпрограмма','SubProgram')
-        ,(3,'Проект','Project')
-        ,(4,'Контракт','Contract')
-        ,(5,'КТ','Point')
-    ) d(activitytypeid, name, sysname)
-    where not exists(
-    	select 1 from activitytype t
-        where t.activitytypeid = d.activitytypeid
-    );
+insert into activitytype
+select d.* from (values 
+	(1,'Программа', 	'program'	),
+   	(2,'Подпрограмма',	'subprogram'),
+	(3,'Проект',		'project'	),
+	(4,'Контракт',		'contract'	),
+	(5,'КТ',			'point'		)
+) as d(id, name, sysname)
+where not exists(
+    select 1 from activitytype as t
+    where t.id = d.id
+);
+
+insert into activity 
+values	(1, 1, 'first_code', 'name_1', 1, null),
+		(2, 2, 'second_code', 'name_2', 1, 1),
+		(3, 3, 'third_code', 'name_3', 1, 2),
+		(4, 4, 'fourth_code', 'name_4', 1, 3),
+		(5, 5, 'fifth_code', 'name_5', 1, 4),
+		(6, 1, 'code1_1', 'name1_1', 2, 1),
+		(7, 2, 'code2_1', 'name2_1', 2, 2),
+		(8, 3, 'code3_1', 'name3_1', 2, 3),
+		(9, 4, 'code4_1', 'name4_1', 2, 4)
+on conflict do nothing;
+
+insert into program 
+values 	(1, 123, 2000, 2001),
+	   	(2, 234, 2000, 2001)
+on conflict do nothing;
+
+insert into subprogram 
+values	(1, 1231),
+		(2, 1232)
+on conflict do nothing;
+
+insert into area 
+values	(1, 'First'),
+		(2, 'Second')
+on conflict do nothing;
+
+insert into project
+values 	(1, 'First project'),
+		(2, 'Second project')
+on conflict do nothing;
+
+insert into contract 
+values 	(1, 1),
+		(2, 2)
+on conflict do nothing;
+
+insert into point
+values	(1, date '2000-01-01', date '2000-02-02'),
+		(2, date '2000-03-03', date '2000-04-04')
+on conflict do nothing;
